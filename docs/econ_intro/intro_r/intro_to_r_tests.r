60da18bbd7c62b3ea78d889f6d4c33461fc44b72
Jonathan Graves <65632825+jlgraves-ubc@users.noreply.github.com>
2023-09-17 22:43:57 -0700

    update renderable notebooks
    - fixing notebook formats so they can be exported more easily to HTML
    - fixing tests for Intro to R

--- docs/econ_intro/intro_r/intro_to_r_tests.r
+++ docs/econ_intro/intro_r/intro_to_r_tests.r
@@ -3,28 +3,28 @@ library(digest)
 
 test_1 <- function() {
   test_that("Solution is incorrect", {
-    expect_equal(digest(answer1), "d110f00cfb1b248e835137025804a23b")
+    expect_equal(digest(answer1), "8ae1ac7bdf62dca7c19b427a9153445c")
   })
   print("Success!")
 }
 
 test_2 <- function() {
   test_that("Solution is incorrect", {
-    expect_equal(digest(answer2), "6e7a8c1c098e8817e3df3fd1b21149d1")
+    expect_equal(digest(answer2), "11946e7a3ed5e1776e81c0f0ecd383d0")
   })
   print("Success!")
 }
 
 test_3 <- function() {
   test_that("Solution is incorrect", {
-    expect_equal(digest(answer3), "38ce1fe9e19a222505e693e8bdd8aeec")
+    expect_equal(digest(answer3), "9239c01218000016670da90e840448be")
   })
   print("Success!")
 }
 
 test_4 <- function() {
   test_that("Solution is incorrect", {
-    expect_equal(digest(answer4), "234fd2733050ae61658f84b0144210b0")
+    expect_equal(digest(answer4), "956c89af90f9cdb78602f16f511bb248")
   })
   print("Success!")
 }
@@ -39,7 +39,14 @@ test_5 <- function() {
 
 test_6 <- function() {
   test_that("Solution is incorrect", {
-    expect_equal(digest(round(answer6, digits = 2)), "90e5b5628ced60ec74b53a4d5527d4a1")
+    expect_equal(digest(round(answer6, digits = 2)), "88d2821508a6625b093500c3e4d8d684")
+  })
+  print("Success!")
+}
+
+test_7 <- function() {
+  test_that("Solution is incorrect", {
+    expect_equal(digest(answer7), "fef8ee3e709a216af937131dec2cff3a")
   })
   print("Success!")
 }
